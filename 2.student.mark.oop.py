f10bd2d9c3307f16cbd08f9e4d8dd8fb63fe2ec7
Tired <Tired@MacBook-Air-cua-lethaihoa.local>
2023-03-13 14:02:23 +0700

    Finished labwork 2

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,148 +1,124 @@
-#Student manager system using oops concept
-#University class for polymorphism
+# University class for polymorphism
 class University:
     def _print(self):
         pass
-#Student class
+
+# Student class
 class Student(University):
     def __init__(self, name, id, dob):
-        self.__name = name
-        self.__id = id
-        self.__dob = dob
-    def _printStudent(self):
-        print("Name: ", self.__name)
-        print("ID: ", self.__id)
-        print("DOB: ", self.__dob)
-    def _getIdStudent(self):
-        return self.__id
-    def _getNameStudent(self):
-        return self.__name
+        self.name = name
+        self.id = id
+        self.dob = dob
+    
+    def __str__(self):
+        return f"Name: {self.name}, ID: {self.id}, DOB: {self.dob}"
 
-
-#Course class
+# Course class    
 class Course(University):
-    
     def __init__(self, name, id):
-        self.__name = name
-        self.__id = id
-        self.__mark = {}
+        self.name = name
+        self.id = id
+        self.marks = {}
     
-    def _printCourse(self):
-        print("Name: ", self.__name)
-        print("ID: ", self.__id)
-        print("Mark: ", self.__mark)
+    def __str__(self):
+        return f"Name: {self.name}, ID: {self.id}, Marks: {self.marks}"
     
-    def _getIdCourse(self):
-        return self.__id
-    
-    def _getNameCourse(self):
-        return self.__name
-    
-    def _getMarkCourse(self):
-        return self.__mark
-    
-    def _setMarkCourse(self, mark):
-        self.__mark.update({Student: mark})
-    
-    def _showMarkCourse(self):
-        for (student,mark) in self.__mark.items():
-            print(f"Student: {student}")
-            print(f"Mark:  {mark}")
+    def set_marks(self, student, mark):
+        self.marks[student] = mark
+        
+    def show_marks(self):
+        for student, mark in self.marks.items():
+            print(student)
+            print(f"Mark: {mark}")
 
-
-#Create student list
-StudentList = []    
-#Input student info
-def inputStudent():
-    NumberOfStudent = int(input("Enter number of student: "))
-    for i in range(NumberOfStudent):
+# Create student list
+students = []
+def input_students():
+    num_of_students = int(input("Enter number of students: "))
+    for i in range(num_of_students):
         name = input("Enter student name: ")
         id = input("Enter student id: ")
         dob = input("Enter student dob: ")
         
-        StudentList.append(Student(name, id, dob))
+        students.append(Student(name, id, dob))
         print("Student added successfully")
 
-#Create course list
-CourseList = []
-
-#Input course info
-def inputCourse():
-    NumberOfCourse = int(input("Enter number of course: "))
-    for i in range(NumberOfCourse):
-        name = input("Enter course name: ")
+# Create course list
+courses = []  
+def input_courses():
+    num_of_courses = int(input("Enter number of courses: "))
+    for i in range(num_of_courses):
+        name = input("Enter course name: ") 
         id = input("Enter course id: ")
-        CourseList.append(Course(name, id))
+        courses.append(Course(name, id))
         print("Course added successfully")
-
-#input mark for student
-def inputMark():
+        
+# Input marks for student
+def input_marks():
     id = input("Enter course id: ")
     found = False
-    for course in CourseList:
-        if course._getIdCourse() == id:
+    for course in courses:
+        if course.id == id:
             found = True
-            for student in StudentList:
-                mark = int(input(f"Enter mark for {student._getNameStudent()}: "))
-                course._setMarkCourse({student: mark})
+            for student in students:
+                mark = int(input(f"Enter mark for {student.name}: "))
+                course.set_marks(student, mark)
             print("Marks added successfully")
             break
     if not found:
         print("Course not found!!!!!")
         print("Input valid course id >:( ")
 
-
-        
-
-#Show student info
-def showStudent():
+# Show student info
+def show_students():
     print("Student list:")
-    for student in StudentList:
-        student._printStudent()
+    for student in students:
+        print(student)
 
-#Show course info
-def showCourse():
+# Show course info
+def show_courses():
     print("Course list:")
-    for course in CourseList:
-        course._printCourse()
+    for course in courses:
+        print(course)
 
-#Show mark for student
-def showMark():
+# Show marks for student
+def show_marks():
     id = input("Enter course id to show marks: ")
     found = False
-    for course in CourseList:
-        if course._getIdCourse() == id:
+    for course in courses:
+        if course.id == id:
             found = True
             print("Marks:")
-            course._showMarkCourse()
+            course.show_marks()
             break
     if not found:
         print("Course not found!!!!!")
         print("Input valid course id >:( ")
-
-
-
-#main
-inputStudent()
-inputCourse()
+          
+# Main program
+input_students()
+input_courses()
 while True:
-    print("Select option:")
+    print("\nSelect option:")
     print("1. Input mark")
     print("2. Show student")
     print("3. Show course")
-    print("4.Show mark")
+    print("4. Show mark")
     print("5. Exit")
-    option = int(input("Enter option: "))
-    if option == 1:
-        inputMark()
-    elif option == 2:
-        showStudent()
-    elif option == 3:
-        showCourse()
-    elif option == 4:
-        showMark()
-    elif option == 5:
-        break
-
     
-    
+    try:
+        option = int(input("Enter option: "))
+        if option == 1:
+            input_marks()
+        elif option == 2:
+            show_students()
+        elif option == 3:
+            show_courses()
+        elif option == 4:
+            show_marks()
+        elif option == 5:
+            break
+        else:
+            print("Invalid option! Please input valid choice!")
+    except ValueError:
+        print("Invalid value entered! Please enter the correct value with specified TYPE i.e. INTEGER etc!")
